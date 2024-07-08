b4251e394637f85b6afadcb33fb7ba8c3cb915e4
Ismailsfar7 <118299197+Ismailsfar7@users.noreply.github.com>
2024-07-08 15:18:44 -0500

    adding numbers using functions

--- prac.sh
+++ prac.sh
@@ -1,5 +1,15 @@
-ADD(){
-  a=2+2
+#!/bin/bash
+
+# Define the function to add two numbers
+add() {
+    local num1=$1
+    local num2=$2
+    local result=$((num1 + num2))
+    echo $result
 }
-ADD
-echo a
+
+# Call the function with the numbers 2 and 2
+result=$(add 2 2)
+
+# Print the result
+echo "The result of 2 + 2 is: $result"
